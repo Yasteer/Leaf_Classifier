70494b50d51fc514f9da9d75ae6cc987153d08c3
Yasteer Sewpersad <30473531+Yasteer@users.noreply.github.com>
2021-03-25 15:44:25 +0200

    Add files via upload

---
+++ Leaf_Classifier.py
@@ -0,0 +1,36 @@
+## Standard Python Libraries ##
+import os, sys
+import numpy as np
+
+## OpenCV Libraries ##
+import cv2, glob
+
+## Keras Libraries ##
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import image_dataset_from_directory
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
+## Dataset Preprocessing ##
+width  = 550
+height = 550
+image_size = (width,height)
+batch_size = 32
+
+images = glob.glob("*.jpg")
+for image in images:
+    img = cv2.imread(image, 0)
+    resized_img = cv2.resize(image,image_size,interpolation = cv2.INTER_AREA)
+    cv2.imwrite(img,resized_img)
+
+## Generate Datasets ##
+Dataset_Dir     = os.path.join("100_Leaves","data")
+Train_DS        = image_dataset_from_directory(directory=Dataset_Dir,validation_split=0.25,subset="training"  ,image_size=image_size,batch_size=batch_size)
+Validation_DS   = image_dataset_from_directory(directory=Dataset_Dir,validation_split=0.25,subset="validation",image_size=image_size,batch_size=batch_size)
+
+## Build Model ##
+model = Sequential()
+
+
+## Train Model ##
